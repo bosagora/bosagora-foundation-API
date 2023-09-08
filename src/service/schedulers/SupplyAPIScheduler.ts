477d11fdb51921835b5be054e87ce9f457993893
HyeonyeobKim <kimhyunb@gmail.com>
2023-09-08 15:47:36 +0900

    Change the supply calculation method

--- src/service/schedulers/SupplyAPIScheduler.ts
+++ src/service/schedulers/SupplyAPIScheduler.ts
@@ -14,11 +14,13 @@ export class SupplyAPIScheduler extends Scheduler {
     private readonly BridgeLiquidity: BigNumber; // Bridge Liquidity Supply
     private readonly BridgeFoundationLiquidity: BigNumber; // Bridge Foundation Stake Amount
     private readonly FoundationAddress: string;
-    private readonly MarketingAddress: string;
+    private readonly MarketingAddress1: string;
+    private readonly MarketingAddress2: string;
     private readonly BountyAddress: string;
     private readonly TeamMemberAddress: string;
     private readonly BurnAddress: string;
     private readonly CommonsBudgetAddress: string; // Agora mainnet
+    private readonly DWFTransferAddress: string;
     private readonly HoledAirdropAddress: Array<string>;
 
     constructor() {
@@ -31,11 +33,13 @@ export class SupplyAPIScheduler extends Scheduler {
         this.BridgeLiquidity = BigNumber.from("10000000000000");
 
         this.FoundationAddress = "0x2529379ac2c209058adf4c28f2c963878ea5e7bd";
-        this.MarketingAddress = "0x4327bb17a6408d8ff94c7be88c20c521ad85d6d7";
+        this.MarketingAddress1 = "0xb18ff7999757fe1e00ced454927b39812f3418aa";
+        this.MarketingAddress2 = "0x4327bb17a6408d8ff94c7be88c20c521ad85d6d7";
         this.BountyAddress = "0x30e5794f87003b15a40827be2cc1c2ae4bc79435";
         this.TeamMemberAddress = "0xabf16eafac1f269a97935b4e3f7e158b61ead3f3";
         this.BurnAddress = "0x000000000000000000000000000000000000dead";
         this.CommonsBudgetAddress = "0x71D208bfd49375285301343C719e1EA087c87b43";
+        this.DWFTransferAddress = "0x12eC499895590898FDf92CA71AcEcCfF33C257C0";
 
         this.HoledAirdropAddress = [
             "0x2e650da344c6fa949962a139cbde6f411b369aba",
@@ -47,7 +51,7 @@ export class SupplyAPIScheduler extends Scheduler {
             "0xd02d41853ad45adce2efce07d5f0982091cf4c6f",
             "0xd62ad9fd3b34813ce5652f04551d6510d09bc75d",
             "0x82394244b86241ef776ccd9948cea0e5dd1f62f8",
-            "0xb18ff7999757fe1e00ced454927b39812f3418aa",
+            // "0xb18ff7999757fe1e00ced454927b39812f3418aa", MarketingAddress1 로 변경
             "0x02e5633f50d89854c6734cade0c9f1b0dc75ce5e",
         ];
 
@@ -67,12 +71,16 @@ export class SupplyAPIScheduler extends Scheduler {
             console.log(`* InitialSupply: ${this.InitialSupply}`);
             const FoundationBalance = BigNumber.from(await this.BOAcontract.balanceOf(this.FoundationAddress));
             console.log(`FoundationBalance: ${FoundationBalance}`);
-            const MarketingBalance = BigNumber.from(await this.BOAcontract.balanceOf(this.MarketingAddress));
-            console.log(`MarketingBalance: ${MarketingBalance}`);
+            const Marketing1Balance = BigNumber.from(await this.BOAcontract.balanceOf(this.MarketingAddress1));
+            console.log(`Marketing1Balance: ${Marketing1Balance}`);
+            const Marketing2Balance = BigNumber.from(await this.BOAcontract.balanceOf(this.MarketingAddress2));
+            console.log(`Marketing2Balance: ${Marketing2Balance}`);
             const BountyBalance = BigNumber.from(await this.BOAcontract.balanceOf(this.BountyAddress));
             console.log(`BountyBalance: ${BountyBalance}`);
             const TeamMemberBalance = BigNumber.from(await this.BOAcontract.balanceOf(this.TeamMemberAddress));
             console.log(`TeamMemberBalance: ${TeamMemberBalance}`);
+            const DWFTransferBalance = BigNumber.from(await this.BOAcontract.balanceOf(this.DWFTransferAddress));
+            console.log(`DWFTransferBalance: ${DWFTransferBalance}`);
             const BurnedBalance = BigNumber.from(await this.BOAcontract.balanceOf(this.BurnAddress));
             console.log(`BurnedBalance: ${BurnedBalance}`);
 
@@ -98,13 +106,15 @@ export class SupplyAPIScheduler extends Scheduler {
             let TotalSupply = this.InitialSupply.add(RewardBalance).add(CommonsBudgetBalance).sub(BurnedBalance);
 
             let CirculatingSupply = TotalSupply.sub(
-                FoundationBalance.add(MarketingBalance)
+                FoundationBalance.add(Marketing1Balance)
+                    .add(Marketing2Balance)
                     .add(BountyBalance)
                     .add(this.BridgeLiquidity)
                     .add(this.BridgeFoundationLiquidity)
                     .add(TeamMemberBalance)
                     .add(HoledAirdropBalance)
                     .add(CommonsBudgetBalance)
+                    .add(DWFTransferBalance)
             );
 
             if (TotalSupply.gte(BigNumber.from(storage.totalSupply))) {
